bc0d7d77eedfa28aae60d32232cb6e01538d5751
sanchewy <sanchewy@gmail.com>
2017-02-11 13:21:42 -0700

    Mouse click now prints pixel value.

--- vlab_2.py
+++ vlab_2.py
@@ -1,29 +1,35 @@
 # Vision Lab 2	   Keinan Balsam
 def change_slider(value):
     pass
-	#print("do something")
+	#print(do something)
 
 import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Sliders", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-#cap.set(cap, cv2.CV_CAP_PROP_FRAME_WIDTH, 480)
-#cap.set(cap, cv2.CV_CAP_PROP_FRAME_HEIGHT, 640)
+cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Sliders', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
+#cv2.resizeWindow('Video', 600, 600)
+#cv2.resizeWindow('Frame', 600, 600)
+#cv2.resizeWindow('Sliders', 600, 600)
+cv2.createTrackbar('Red', 'Sliders', 255, 255, change_slider)
+cv2.createTrackbar('Green', 'Sliders', 255, 255, change_slider)
+cv2.createTrackbar('Blue', 'Sliders', 255, 255, change_slider)
 
-cv2.createTrackbar("change", "Sliders", 255, 255, change_slider)
+def get_mouse(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        px = frame[y ,x]
+        print(px)
+        
+cv2.setMouseCallback('Frame',get_mouse)
+
 while True:
 	ret, frame = cap.read()
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	cv2.imshow("Frame", hsv)
-	
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	cv2.imshow('Frame', frame)
 	status, img = cap.read()
-	cv2.imshow("Video", img)
-	cv2.resizeWindow("Video", 600, 600)
-	cv2.resizeWindow("Frame", 600, 600)
-	cv2.resizeWindow("Sliders", 600, 600)
+	cv2.imshow('Video', img)
 	k = cv2.waitKey(33)
 	if k == ord('q'):
 		break
