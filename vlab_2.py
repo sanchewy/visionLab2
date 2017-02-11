ceb9bb7e6565f83fc0588d8aa32b7dbf8e9fee8d
sanchewy <sanchewy@gmail.com>
2017-02-11 12:15:04 -0700

    I added the HSV support window.

--- vlab_2.py
+++ vlab_2.py
@@ -1,23 +1,31 @@
-# Vision Lab 2 	Keinan Balsam
+# Vision Lab 2	   Keinan Balsam
 def change_slider(value):
-	print("do something")
+    pass
+	#print("do something")
 
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CV_CAP_PROP_FRAME_HEIGHT, 640)
-cap.set(cv2.CV_CAP_PROP_FRAME_WIDTH, 480)
 cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
 cv2.namedWindow("Sliders", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
+#cap.set(cap, cv2.CV_CAP_PROP_FRAME_WIDTH, 480)
+#cap.set(cap, cv2.CV_CAP_PROP_FRAME_HEIGHT, 640)
 
 cv2.createTrackbar("change", "Sliders", 255, 255, change_slider)
 while True:
-    status, img = cap.read()
-    cv2.imshow("Video", img)
-    cv2.resizeWindow("Video", 600,600)
-    cv2.resizeWindow("Sliders", 600,600)
-    k = cv2.waitKey(33)
-    if k == ord('q'):
-        break
+	ret, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	cv2.imshow("Frame", hsv)
+	
+	status, img = cap.read()
+	cv2.imshow("Video", img)
+	cv2.resizeWindow("Video", 600, 600)
+	cv2.resizeWindow("Frame", 600, 600)
+	cv2.resizeWindow("Sliders", 600, 600)
+	k = cv2.waitKey(33)
+	if k == ord('q'):
+		break
 cap.release()
 cv2.destroyAllWindows()
