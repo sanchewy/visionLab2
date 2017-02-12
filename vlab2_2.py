aca6b9800156489bc905955be6d48db3f99d7b40
sanchewy <sanchewy@gmail.com>
2017-02-11 20:25:14 -0700

    Took main recording then: equalized, blured, and 32 bit.

--- vlab2_2.py
+++ vlab2_2.py
@@ -9,18 +9,29 @@ cap = cv2.VideoCapture(0)
 
 #Define windows
 cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Equalize', cv2.WINDOW_AUTOSIZE)
 
 #Define main funciton runloop
 while True:
-    #Set up BRG live video
+	#Set up BRG live video
 	status, img = cap.read()
 	cv2.imshow('Video', img)
-    
-    #Exit on keyboard 'q' press
+	
+	#Create new frame and brighten
+	imgyuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+	imgyuv[:,:,0] = cv2.equalizeHist(imgyuv[:,:,0])
+	equalize = cv2.cvtColor(imgyuv, cv2.COLOR_YUV2BGR)
+	
+	#Now blur equalize and turn 32 bit
+	kernel = np.ones((5,5),np.float32)/25
+	dst = cv2.filter2D(equalize,-1,kernel)
+	cv2.imshow('Equalize', equalize)
+	
+	#Exit on keyboard 'q' press
 	k = cv2.waitKey(33)
 	if k == ord('q'):
 		break
-        
+		
 #Define exit strategy
 cap.release()
 cv2.destroyAllWindows()
